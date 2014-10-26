327da4e064867742b19d87d880a62f0af1b8a590
Jak Spalding <jak@jak.io>
2014-10-26 23:22:36 +0000

    the clock

---
+++ clock/client.rb
@@ -0,0 +1,3 @@
+require 'comboclient'
+
+# This used to contain the client, it's now moved to its own gem.
---
+++ clock/listeningclock.rb
@@ -0,0 +1,18 @@
+trap("SIGINT") { exit! }
+
+require './client'
+
+puts "This is The Clock"
+
+time = 0
+
+timetopic = ComboClient.new('178.62.106.39', 'time')
+displaytopic = ComboClient.new('178.62.106.39', 'display.rendered')
+
+timetopic.add(time: time)
+
+displaytopic.subscribe do |newdata, manager|
+  time += 1
+  puts "TICK! The time is now #{time}"
+  timetopic.add(time: time)
+end
